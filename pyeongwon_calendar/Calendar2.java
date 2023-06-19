b95fb2d5073ceecf67c35aa21e9cbb5183f8d069
OWNER <OWNER@192.168.219.194>
2023-06-19 16:36:58 +0900

    for문을 이용한 최대일수 출력

--- pyeongwon_calendar/Calendar2.java
+++ pyeongwon_calendar/Calendar2.java
@@ -1,31 +1,40 @@
 package pyeongwon_calendar;
+
 import java.util.Scanner;
-public class Calendar2{
-	private static final int[] MAX_DAYS ={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	public static int getMaxDaysofMonth(int month) {
-		return MAX_DAYS[month -1];
+
+public class Calendar2 {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
 	}
-	public static void printCalendar() {
-		
-		System.out.println(" ");
+
+	public static void PrintCalendar() {
+
+		System.out.println("");
 		System.out.println(" 일 월  화  수  목 금  토 ");
 		System.out.println("---------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
+
 	}
-		public static void main(String[] args) {
-		//특정달의 최대일수 출력하기.
-		System.out.println("달을 입력하세요.");
+
+	public static void main(String[] args) {
+		// 숫자를 입력받아 입력받은 횟수만큼 최대일수 달을 출력.
 		Scanner scanner = new Scanner(System.in);
-		int month = scanner.nextInt();
 		Calendar2 cal = new Calendar2();
 		
-		System.out.printf("%d월의 최대일수는 %d일입니다.\n", month, cal.getMaxDaysofMonth(month));
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+		
+		for ( int i = 1; i <= repeat; i++ ) {
+			System.out.println("달을 입력하세요.");
+			int month = scanner.nextInt();
+			System.out.printf("%d달의 최대일수는 %d입니다.\n", month, cal.getMaxDaysOfMonth(month));
+		}
+		System.out.println("THE END");
 		scanner.close();
-		cal.printCalendar();
-	
 	}
 }
