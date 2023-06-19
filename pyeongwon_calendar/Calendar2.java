1bc027a0d663531333b43c3b4529c7442eeec8ae
OWNER <OWNER@192.168.219.194>
2023-06-19 14:56:19 +0900

    특정달의 최대일수 구하기 2(method)

--- pyeongwon_calendar/Calendar2.java
+++ pyeongwon_calendar/Calendar2.java
@@ -1,14 +1,31 @@
 package pyeongwon_calendar;
 import java.util.Scanner;
 public class Calendar2{
-	public static void main(String[] args) {
+	private static final int[] MAX_DAYS ={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public static int getMaxDaysofMonth(int month) {
+		return MAX_DAYS[month -1];
+	}
+	public static void printCalendar() {
+		
+		System.out.println(" ");
+		System.out.println(" 일 월  화  수  목 금  토 ");
+		System.out.println("---------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		
+	}
+		public static void main(String[] args) {
 		//특정달의 최대일수 출력하기.
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
 		int month = scanner.nextInt();
-		int [] MAX_DAYS= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.printf("%d월의 최대일수는 %d일입니다.", month, MAX_DAYS[month - 1]);
+		Calendar2 cal = new Calendar2();
+		
+		System.out.printf("%d월의 최대일수는 %d일입니다.\n", month, cal.getMaxDaysofMonth(month));
 		scanner.close();
+		cal.printCalendar();
+	
 	}
 }
-
